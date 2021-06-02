ba38813198e8f3215c03e68f679f17a5b36428ce
AGardey <agustingardey@gmail.com>
2021-06-02 16:34:22 -0300

    se corrigó app.set y app.liste

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ let express = require('express');
 let path = require('path');
 let app = express ();
 
+app.set("port", process.env.PORT || 3000)
+
+app.listen (app.get("port"),() => console.log('Server start http://localhost'+app.get("port")));
+
 app.use(express.static(path.resolve(__dirname,"./public")));
-app.listen (3000, () => console.log('Server start in http://localhost:3000'));
+
 app.get('/', function (req, res){
     res.sendFile(path.resolve(__dirname, './views/', 'home.html'));
 
